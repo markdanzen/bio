849aa352965474f511503b61b36289d3a7ca4ff7
Danzen <markdanzen@gmail.com>
2026-08-24 14:53:08 +0800

    swup integration

--- src/components/SwupProvider.tsx
+++ src/components/SwupProvider.tsx
@@ -2,12 +2,14 @@
 
 import { useEffect } from "react";
 import Swup from "swup";
+import SwupHeadPlugin from "@swup/head-plugin";
 
 export default function SwupProvider() {
   useEffect(() => {
     const swup = new Swup({
       containers: ["#swup"],
       animationSelector: '[class*="transition-"]',
+      plugins: [new SwupHeadPlugin({ awaitAssets: true })],
     });
 
     return () => {
